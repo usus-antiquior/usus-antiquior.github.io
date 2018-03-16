bea500a4f02d5f23053ffa585328bab0e33d15a9
usus-antiquior <37345019+usus-antiquior@users.noreply.github.com>
2018-03-16 10:52:15 +0100

    sw

--- sw.js
+++ sw.js
@@ -3,14 +3,24 @@ self.addEventListener('install', function(e) {
    caches.open('ususantiquior').then(function(cache) {
      return cache.addAll([
        '/',
-       '/index.html'
-       '/css/grayscale.css'
+       '/index.html',
+       '/css/grayscale.css',
        '/js/grayscale.min.js'
      ]);
    })
  );
 });
 
-self.addEventListener('fetch', function(e){
-
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
 });
