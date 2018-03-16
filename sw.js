897250d72ea5906ae6ffdc4741de604358c41970
usus-antiquior <37345019+usus-antiquior@users.noreply.github.com>
2018-03-16 08:37:25 +0100

    add service worker

---
+++ sw.js
@@ -0,0 +1,10 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html'
+     ]);
+   })
+ );
+});
